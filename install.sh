4a0412f93aea5a3c6419c6f65c494a1b91c4a613
Thomas Preece <thomaspreece10@gmail.com>
2020-08-02 21:27:32 +0100

    feat: allow using curl instead of wget

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 set -e
 
 BASEDIR=$(dirname "$0")
@@ -9,22 +11,45 @@ if [ ! -f setup_worms_armageddon_2.0.0.2.exe ]; then
   exit 1
 fi
 
+if which wget >/dev/null; then
+  echo "Using wget"
+  tool="wget"
+elif which curl >/dev/null; then
+  echo "Using curl"
+  tool="curl"
+else
+  echo "Could not find wget or curl on system"
+  exit 1
+fi
+
 if [ ! -f docker-wine ]; then
-  wget https://raw.githubusercontent.com/scottyhardy/docker-wine/master/docker-wine
+  if [ "$tool" = "wget" ]; then
+    wget https://raw.githubusercontent.com/scottyhardy/docker-wine/master/docker-wine
+  else
+    curl https://raw.githubusercontent.com/scottyhardy/docker-wine/master/docker-wine -o docker-wine
+  fi
   chmod +x docker-wine
 fi
 
 if [ ! -f WA_update-3.8_\[CD\]_Installer.exe ]; then
-  wget https://worms2d.info/files/WA_update-3.8_%5BCD%5D_Installer.exe
+  if [ "$tool" = "wget" ]; then
+    wget https://worms2d.info/files/WA_update-3.8_%5BCD%5D_Installer.exe
+  else
+    curl https://worms2d.info/files/WA_update-3.8_%5BCD%5D_Installer.exe -o WA_update-3.8_[CD]_Installer.exe
+  fi
 fi
 
 if [ ! -f wkWormNAT2-tfgdbproxy.zip ]; then
-  wget https://github.com/daves-studio/MyWormNET2/releases/download/v1.0.1/wkWormNAT2-tfgdbproxy.zip
+  if [ "$tool" = "wget" ]; then
+    wget https://github.com/daves-studio/MyWormNET2/releases/download/v1.0.1/wkWormNAT2-tfgdbproxy.zip
+  else
+    curl https://github.com/daves-studio/MyWormNET2/releases/download/v1.0.1/wkWormNAT2-tfgdbproxy.zip -o wkWormNAT2-tfgdbproxy.zip
+  fi
 fi
-
-# Initialise .wine folder
-./docker-wine wine cmd.exe /C "echo Initialised .wine folder"
-
-# Install WA
-# Have to run as root as we need to use apt in container 
-./docker-wine --volume="$(pwd):/home/wineuser/WA/" --as-root /bin/bash -c "/home/wineuser/WA/WA-install.sh"
+#
+# # Initialise .wine folder
+# ./docker-wine wine cmd.exe /C "echo Initialised .wine folder"
+#
+# # Install WA
+# # Have to run as root as we need to use apt in container
+# ./docker-wine --volume="$(pwd):/home/wineuser/WA/" --as-root /bin/bash -c "/home/wineuser/WA/WA-install.sh"
